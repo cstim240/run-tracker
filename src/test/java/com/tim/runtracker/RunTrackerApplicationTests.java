be60003fc7f07e1ac1967f6ea04da7533f64edc5
cstim240 <timsupann@gmail.com>
2025-09-27 11:08:11 -0700

    Initial Spring Boot project setup from spring initializer

---
+++ src/test/java/com/tim/runtracker/RunTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.runtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
