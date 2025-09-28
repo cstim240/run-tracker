081112b4c730767ac28da59d7b5e6cb89917c4b1
cstim240 <timsupann@gmail.com>
2025-09-28 16:40:32 -0700

    Add validation dependency annotations for basic validation checks

--- src/main/java/com/tim/runtracker/model/Run.java
+++ src/main/java/com/tim/runtracker/model/Run.java
@@ -3,6 +3,9 @@ package com.tim.runtracker.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
+import org.springframework.lang.NonNull;
 
 import java.time.LocalDate;
 
@@ -16,8 +19,13 @@ import java.time.LocalDate;
 public class Run {
     @Id @GeneratedValue
     private Long id;
+
+    @NonNull
+    @PastOrPresent(message = "input date cannot happen in the future")
     private LocalDate date;
+    @NonNull @Positive(message = "distance cannot be negative")
     private Double distance;
+    @NonNull @Positive(message = "duration cannot be negative")
     private Integer duration;
 
     // constructors, getters, setters
